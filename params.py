c6d285fe2732870524d151df6d62d9e797ad3006
anhkhoado93 <45436284+anhkhoado93@users.noreply.github.com>
2022-03-20 12:01:24 +0700

    save all

--- params.py
+++ params.py
@@ -17,7 +17,7 @@ PATH_PRETRAINED_MODEL='autocorrection/weights/model/model_last.pth'
 IS_SPLIT_INDEXES=True
 
 # Number epoches
-N_EPOCH=3
+N_EPOCH=10
 
 # lambda  for control the important level of detection loss value
 LAMDA=0.6
